b36822ab94a157bdadd491e9b9e035e09710e62a
chad-helbling <chad.helbling.1@gmail.com>
2023-08-07 23:10:11 -0500

    feat: updates some styles

--- src/components/PaginationBar.tsx
+++ src/components/PaginationBar.tsx
@@ -11,21 +11,21 @@ type PaginationBarProps = {
 };
 
 export function PaginationBar({
-  crewPerPage,
-  totalCrew,
-  visibleCrew,
-  indexOfFirstCrew,
-  currentPage,
-  paginate,
-}: PaginationBarProps) {
+                                crewPerPage,
+                                totalCrew,
+                                visibleCrew,
+                                indexOfFirstCrew,
+                                currentPage,
+                                paginate,
+                              }: PaginationBarProps) {
   const totalPages = Math.ceil(totalCrew / crewPerPage);
   //make array not zero based for page numbers
-  const pageNumbers = [...Array(totalPages).keys()].map((element, index) => index + 1);
+  const pageNumbers = [ ...Array(totalPages).keys() ].map((element, index) => index + 1);
 
   const classNameSelected =
-    'relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600';
+    'relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-default';
   const claseNameBase =
-    'relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0';
+    'relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-100 focus:z-20 focus:outline-offset-0 cursor-default';
 
   function paginateHandler(page: number) {
     if (page > totalPages || page < 1) return;
@@ -38,13 +38,13 @@ export function PaginationBar({
       <div className="flex flex-1 justify-between sm:hidden">
         <a
           href="#"
-          className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+          className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100"
         >
           Previous
         </a>
         <a
           href="#"
-          className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+          className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100"
         >
           Next
         </a>
@@ -53,7 +53,7 @@ export function PaginationBar({
         <div>
           <p className="text-sm text-gray-700">
             Showing{' '}
-            <span data-testid="showing-first-value" className="font-medium">
+            <span data-testid="showing-first-value" className="font-medium ">
               {visibleCrew ? indexOfFirstCrew + 1 : 0}
             </span>{' '}
             to{' '}
@@ -74,7 +74,7 @@ export function PaginationBar({
               className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
               <span className="sr-only">Previous</span>
-              <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+              <ChevronLeftIcon className="h-5 w-5" aria-hidden="true"/>
             </a>
             {pageNumbers?.map((page) => (
               <a
@@ -91,7 +91,7 @@ export function PaginationBar({
               className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
               <span className="sr-only">Next</span>
-              <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+              <ChevronRightIcon className="h-5 w-5" aria-hidden="true"/>
             </a>
           </nav>
         </div>
