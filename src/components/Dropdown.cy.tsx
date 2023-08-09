e401c5472a1024b8f6a7f22a0b197a5f5832ec94
chad-helbling <chad.helbling.1@gmail.com>
2023-08-08 19:33:22 -0500

    chore: adds cypress dir for build

--- src/components/Dropdown.cy.tsx
+++ src/components/Dropdown.cy.tsx
@@ -3,10 +3,12 @@ import { Dropdown } from './Dropdown';
 
 describe('<Dropdown />', () => {
   it('renders', () => {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
     cy.mount(<Dropdown onChange={() => {}} />);
   });
 
   it('clicks NASA', () => {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
     cy.mount(<Dropdown onChange={() => {}} />);
 
     cy.get('[data-testid="dropdown-button"]').click();
